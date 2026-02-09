13297dd0ea487a1cb2cefd590d3cf4034a910897
Alejandro Jimenez Garcia <alejandj@c1r16s5.42madrid.com>
2026-02-09 17:41:39 +0100

    first commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/09 14:15:46 by alejandj          #+#    #+#             */
+/*   Updated: 2026/02/09 17:36:30 by alejandj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+char	ft_toupper(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+} 
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	int i = 1;
+	
+	while (i < argc)
+	{
+		int j = 0;
+		while (argv[i][j])
+		{
+			std::cout << ft_toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
