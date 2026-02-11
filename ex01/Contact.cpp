c43b6cd7f153f44ff2d5f0b042f7a1609997af19
AlejandroJimenez16 <alejanjiga@gmail.com>
2026-02-11 17:01:59 +0100

    Clase Contact

---
+++ ex01/Contact.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 14:03:18 by alejandj          #+#    #+#             */
+/*   Updated: 2026/02/11 17:01:30 by alejandj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+Contact::Contact(
+    const std::string first_name,
+	const std::string last_name,
+	const std::string nickname,
+	const std::string phone_number,
+	const std::string darkest_secret
+)
+{
+    this->first_name = first_name;
+    this->last_name = last_name;
+    this->nickname = nickname;
+    this->phone_number = phone_number;
+    this->darkest_secret = darkest_secret;
+}
+
+std::string Contact::get_first_name()
+{
+    return first_name;
+}
+
+std::string Contact::get_last_name()
+{
+    return last_name;
+}
+
+std::string Contact::get_nickname()
+{
+    return nickname;
+}
+
+std::string Contact::get_phone_number()
+{
+    return phone_number;
+}
+
+std::string Contact::get_darkest_secret()
+{
+    return darkest_secret;
+}
---
+++ ex01/Contact.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 13:44:13 by alejandj          #+#    #+#             */
+/*   Updated: 2026/02/11 17:01:18 by alejandj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+#include <iostream>
+
+class Contact
+{
+	private:
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nickname;
+		std::string	phone_number;
+		std::string	darkest_secret;
+	public:
+		Contact();
+		Contact(
+			const std::string first_name,
+			const std::string last_name,
+			const std::string nickname,
+			const std::string phone_number,
+			const std::string darkest_secret
+		);
+		std::string get_first_name();
+		std::string get_last_name();
+		std::string get_nickname();
+		std::string get_phone_number();
+		std::string get_darkest_secret();
+};
+
+#endif
